67fccd31f1a574b1f8c00f08f893c43cb6317bf0
LiuVII <mpyblake@gmail.com>
2018-10-06 01:43:49 -0700

    Add pre-push githook for python files codestyle

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import os
 
+# Configure git to use githooks directory
+os.system("git config core.hooksPath ./buildsupport/githooks/")
+
 # Install pipenv
 if os.system("pip3 install pipenv") and os.system("pip install pipenv"):
     print("Couldn't locate neither pip nor pip3 commands, please install pip")
